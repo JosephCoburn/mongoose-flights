4a13027d588840675e10f97b74aa0efe74594007
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-05-19 17:04:35 -0700

    create route and table

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -1,6 +1,34 @@
+const Flight = require('../models/flight');
+
 module.exports = {
-    new: newFlight
+    index,
+    new: newFlight,
+    create
 };
+
+function index(req, res) {
+    Flight.find({}, function(err, flights) {
+        res.render('flights/index', {
+            flights
+        });
+    }); 
+};
+
+function create(req, res) {
+    // convert onTime checkbox to a boolean
+    req.body.onTime = !!req.body.onTime;
+    // remove extra whitespace next to commas
+    req.body.passengers = req.body.passengers.replace(/\s*,\s*/g, ',');
+    // split if passengers is not empty string
+    if (req.body.passengers) req.body.passengers = req.body.passengers.split(',');
+    var flight = new Flight(req.body);
+    flight.save(function(err) {
+        // one way to handle errors in express
+        if (err) return res.render('flights/new');
+        console.log(flight);
+        res.redirect('/flights/new');
+    });
+}
 
 function newFlight(req, res) {
 
--- mongoose-flights/routes/flights.js
+++ mongoose-flights/routes/flights.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
-/* GET users listing. */
+// Get /flights 
+router.get('/', flightsCtrl.index);
+// GET /flights/new
 router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
